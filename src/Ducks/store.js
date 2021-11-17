538ca142c99cd2f28d1149e545811cd8cada4921
jayyrt <jayy.rt19@gmail.com>
2021-11-16 20:03:53 -0700

    at the beginning

---
+++ src/Ducks/store.js
@@ -0,0 +1,11 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import promiseMiddleware from 'redux-promise-middleware';
+import calendarReducer from './calendarReducer';
+import userReducer from './userReducer';
+
+const rootReducer = combineReducers({
+    calendar: calendarReducer,
+    user: userReducer,
+});
+
+export default createStore(rootReducer, applyMiddleware(promiseMiddleware))
