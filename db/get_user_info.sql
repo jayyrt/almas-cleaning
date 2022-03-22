42dfadfef1682d8bc34ba4a878c4d7bbb3413802
jayyrt <jayy.rt19@gmail.com>
2022-03-22 00:45:33 -0600

    get_user_info edit

--- db/get_user_info.sql
+++ db/get_user_info.sql
@@ -1,2 +1,6 @@
-SELECT * FROM user_info
-WHERE userID = $1;
+SELECT ui.userID, ui.name, ui.address, ui.address, ui.city, ui.phone_num
+FROM user_info ui
+-- add JOIN sql here !!!
+-- will be joining login info with user data
+WHERE userID = $1; 
+-- AND auth info
