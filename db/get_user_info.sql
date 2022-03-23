b6f1d0d2a7051b861557a6f88e16fe47b0b1fd13
jayyrt <jayy.rt19@gmail.com>
2022-03-22 19:44:56 -0600

    completed sql

--- db/get_user_info.sql
+++ db/get_user_info.sql
@@ -1,6 +1,6 @@
 SELECT ui.userID, ui.name, ui.address, ui.address, ui.city, ui.phone_num
 FROM user_info ui
--- add JOIN sql here !!!
--- will be joining login info with user data
-WHERE userID = $1; 
--- AND auth info
+JOIN users u ON u.id = ui.userID
+-- joining login info with user data
+WHERE ui.userID = $1
+-- does AND need to be added with auth info
