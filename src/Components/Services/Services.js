94ec526d5f8facb16282ced3de3e56b0de63f3a6
jayyrt <jayy.rt19@gmail.com>
2022-05-06 16:02:22 -0600

    progressing services components

--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -23,17 +23,35 @@ export default class Services extends Component {
                 <h1 className="header">
                     <div className="title">Alma's Cleaning Company</div>
                     <div className="links">
-                        <button className="link-content" onClick={this.getLogin}>Login/Register</button>
+                        <button className="link-content" onClick={this.getLogin}>Login</button>
                     </div>
                 </h1>
+
+            <div>
                 <div className='info-content'>
                     <div className='service-container'>
                         <h2>Services</h2>
+                        <div className='services'>
+                            <div className='service-content'>
+                                <h3>Deep Cleaning</h3>
+                                <p>This service is a deep clean</p>
+                            </div>
+                            <div className='service-content'>
+                                <h3>Daily Cleaning</h3>
+                                <p>This service is a daily clean</p>
+                            </div>
+                            <div className='service-content'>
+                                <h3>Construction Cleaning</h3>
+                                <p>This service is a construction clean</p>
+                            </div>
+                        </div>
                     </div>
-                    <div className='about-container'>
+
+                    <div className='service-container'>
                         <h2>About Company</h2>
                     </div>
                 </div>
+            </div>
             </Background>
         );
     }
