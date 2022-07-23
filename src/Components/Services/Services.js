4ef9b4ed58a54949ae3bf0b1ece1566dde5e03b2
jayyrt <jayy.rt19@gmail.com>
2022-07-23 02:12:17 -0600

    Services Flip Cards

--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactCardFlip from 'react-card-flip';
 import Adrian from './Adrian.jpg';
 import Background from './../Shared/Background/Background';
 // import axios from 'axios';
@@ -7,7 +8,10 @@ import './Services.css';
 export default class Services extends Component {
     constructor(props){
         super(props);
-
+        this.state = {
+            isFlipped: false
+        };
+            this.handleClick = this.handleClick.bind(this);
         // this.getLogin = this.getLogin.bind(this);
     }
 
@@ -18,68 +22,71 @@ export default class Services extends Component {
     // componentDidMount(){
     // }
 
+    handleClick(e) {
+        e.preventDefault();
+        this.setState(prevState => ({ isFlipped: !prevState.isFlipped }));
+    }
+
     render() {
         return (
             <Background>
-            <div>
-                    <div className='service-container'>
-                        <h2>Services</h2>
-                        <div className='services'>
-                            <div className='flip-card'>
-                            <div className='service-flipcard-inner-1'>
-                                <div className='flip-card-front-1'>
-                                    img
-                                </div>
-                                <div className='flip-card-back-1'>
-                                <h3>Deep Cleaning</h3>
-                                <ul>
-                                    <li>Restrooms deep clean</li>
-                                    <li>Detail kitchen</li>
-                                    <li>Dust & polish</li>
-                                    <li>Sweep & mop</li>
-                                </ul>
-                                </div>
-                            </div>
-                            </div>
-                            <div className='flip-card'>
-                            <div className='service-flipcard-inner-2'>
-                                <div className='flip-card-front-2'>
-                                    img
-                                </div>
-                                <div className='flip-card-back-2'>
-                                <h3>Daily Cleaning</h3>
-                                <ul>
-                                    <li>Restrooms deep clean</li>
-                                    <li>Detail kitchen</li>
-                                    <li>Dust & polish</li>
-                                    <li>Sweep & mop</li>
-                                </ul>
-                                </div>
-                            </div>
-                            </div>
-                            <div className='flip-card'>
-                            <div className='service-flipcard-inner-3'>
-                                <div className='flip-card-front-3'>
-                                    img
-                                </div>
-                                <div className='flip-card-back-3'>
-                                <h3>Construction Cleaning</h3>                        
-                                <ul>
-                                    <li>Restrooms deep clean</li>
-                                    <li>Detail kitchen</li>
-                                    <li>Dust & polish</li>
-                                    <li>Sweep & mop</li>
-                                </ul>
-                                </div>
-                            </div>
-                            </div>
-                        </div>
+                <div className='service-container'>
+                <h2>Services</h2>
+                <div className='services'>
+
+                    <ReactCardFlip isFlipped={this.state.isFlipped} flipDirection="horizontal">
+                    <div className='services-content' onClick={this.handleClick}>
+                        <h3>Deep Cleaning</h3>
+                        <ul>
+                            <li>Restrooms deep clean</li>
+                            <li>Detail kitchen</li>
+                            <li>Dust & polish</li>
+                            <li>Sweep & mop</li>
+                        </ul>
                     </div>
+
+                    <div className='services-content' onClick={this.handleClick}>
+                        <img />
+                    </div>
+                    </ ReactCardFlip>
+                    
+                    <ReactCardFlip isFlipped={this.state.isFlipped} flipDirection="horizontal">
+                    <div className='services-content' onClick={this.handleClick}>
+                        <h3>Daily Cleaning</h3>
+                        <ul>
+                            <li>Restrooms deep clean</li>
+                            <li>Detail kitchen</li>
+                            <li>Dust & polish</li>
+                            <li>Sweep & mop</li>
+                        </ul>
+                    </div>
+
+                    <div className='services-content' onClick={this.handleClick}>
+                        <img />
+                    </div>
+                    </ ReactCardFlip>      
+
+                    <ReactCardFlip isFlipped={this.state.isFlipped} flipDirection="horizontal">
+                    <div className='services-content' onClick={this.handleClick}>
+                        <h3>Construction Cleaning</h3>                        
+                        <ul>
+                            <li>Restrooms deep clean</li>
+                            <li>Detail kitchen</li>
+                            <li>Dust & polish</li>
+                            <li>Sweep & mop</li>
+                        </ul>
+                    </div>
+
+                    <div className='services-content' onClick={this.handleClick}>
+                        <img />
+                    </div>
+                    </ ReactCardFlip>   
+                </div>    
+                </div>   
+
                     <div className='img-container'>
                         <img className='adrian' src={Adrian} alt='Team Image' />
-                    </ div>
-
-                </div>
+                    </div>
             </Background>
         );
     }
